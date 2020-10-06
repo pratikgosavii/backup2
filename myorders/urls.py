b270398d9974ee484360bce30df002236b4bd08c
Sushil333 <sushilbhardwaj705@gmail.com>
2020-10-06 11:37:57 +0530

    feature-add: account page

--- myorders/urls.py
+++ myorders/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from myorders import views
 
 urlpatterns = [
-    path('', views.myorders, name='my-orders')
+    path('my-orders', views.myorders, name='my-orders')
 ]
